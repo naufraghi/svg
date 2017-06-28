21039fe436703a7804f07b93239fe83953eb9d2c
Ani Kembhavi <anik@allenai.org>
2017-06-28 13:29:53 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='python_svg_parser',
+      version='0.0.1',
+      description='Python SVG parser forked from cjlano/svg',
+      url='https://github.com/allenai/svg',
+      packages=find_packages(),
+      )
